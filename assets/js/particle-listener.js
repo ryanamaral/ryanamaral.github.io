bef17568b7b4dc449f09d30a90629f86858586e0
Ryan Amaral <me@ryanamaral.com>
2020-06-29 23:52:27 +0100

    added particles

---
+++ assets/js/particle-listener.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+	
+	new SweetScroll({/* some options */});
+
+	particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+	  console.log('Callback - particles js config loaded');
+	});
+
+}, false);
